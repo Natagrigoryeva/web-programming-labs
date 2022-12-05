5c93b9efc8222c5cc41aaae130658412f5643dc6
Григорьева Наталья <nata.grigoryeva.2002@gmail.com>
2022-12-05 16:18:45 +0700

    Alert

--- lab6.js
+++ lab6.js
@@ -39,3 +39,34 @@ function setEnglish() {
 
     document.getElementById('reset-button').value = 'Reset';
 }
+function showAlert() {
+    let userName = document.getElementById('input-name').value;
+    let userSurname = document.getElementById('input-surname').value;
+    let userAge = document.getElementById('input-age').value;
+    alert('Привет, ' + userName + ' ' + userSurname + '\n' + 'Ваш возраст: ' + userAge);
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('user-data').style.backgroundColor = color;
+}
+
+function studentName() {
+    let studentName = document.getElementById('student').innerHTML;
+    alert('Работу выполнила' + ' ' + studentName);
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('student').style.backgroundColor = color;
+}
+
+function infoFooter() {
+    let footer = document.getElementById('footer').innerHTML;
+    alert('Данные:' + ' ' + footer);
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('footer').style.backgroundColor = color;
+}
