c1a46863d6f3fdc8c4ca3e1d2f5bdde664c5f4e8
Григорьева Наталья <nata.grigoryeva.2002@gmail.com>
2022-12-23 10:35:51 +0700

    Вывод координат мыши

--- lab9.js
+++ lab9.js
@@ -31,3 +31,11 @@ function knopka (button) {
     }
 }
 
+function showCoords(event) {
+    event.target.innerHTML = 'x = ' + event.offsetX + ', y = ' + event.offsetY + ' ' + 
+    (event.ctrlKey? '<b>Ctrl</b>': '<s>Ctrl</s>') + ' ' + (event.shiftKey? '<b>Shift</b>': '<s>Shift</s>') + ' ' + (event.altKey? '<b>Alt</b>': '<s>Alt</s>');
+}
+window.addEventListener('DOMContentLoaded', function() {
+    let coords = document.querySelector('#coords');
+    coords.addEventListener('mousemove', showCoords);
+})
