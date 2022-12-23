0972adda6787215a058e9a933da4d51000bb92af
Григорьева Наталья <nata.grigoryeva.2002@gmail.com>
2022-12-23 09:05:25 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -4,4 +4,14 @@ function showDate(params) {
     out.innerHTML = today.toLocaleString('ru-RU');
 }
 
+function showDaysCount(params) {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]')
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    let result = document.getElementById('result');
+    daysCount = Math.ceil(daysCount);
+    result.innerHTML = ('С даты рождения прошло дней: ' + daysCount);
+}
+
 window.addEventListener('load', showDate);
