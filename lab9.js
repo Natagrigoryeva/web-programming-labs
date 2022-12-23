6900d3e819debf994b6826eecda767e056a1ed13
Григорьева Наталья <nata.grigoryeva.2002@gmail.com>
2022-12-23 10:14:09 +0700

    Переключения состояний

--- lab9.js
+++ lab9.js
@@ -11,3 +11,23 @@ function student(button) {
     else
         button.innerHTML = 'Григорьева';
 }
+
+function knopka (button) {
+    if (button.innerHTML == 'Нажми меня') {
+        button.innerHTML = 'Ещё раз нажми';
+        button.style.color = 'blue';
+    }
+    else if (button.innerHTML == 'Ещё раз нажми') {
+        button.innerHTML = 'Ещё';
+        button.style.color = 'orange';
+    }  
+    else if (button.innerHTML == 'Ещё') {
+        button.innerHTML = 'Начать сначала';
+           button.style.color = 'red'; 
+        }
+    else {
+        button.innerHTML = 'Нажми меня';
+        button.style.color = 'pink';
+    }
+}
+
